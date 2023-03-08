a56c7c290ed9eaa32c88e91f657cf0bdbc85c4ee
Bryan Kortman <bryan.kortman@cern.ch>
2023-03-08 14:27:04 +0100

    adding enviroment pipinstall

---
+++ pipinstall.sh
@@ -0,0 +1,11 @@
+# python=3.8
+pip install jupyterhub
+pip install pydot
+pip install graphviz
+pip install jupyter
+pip install tensorflow==2.4.0
+pip install tensorflow_datasets
+pip install hls4ml[profiling]==0.6.0
+pip install qkeras==0.9.0
+pip install scikit-learn
+pip install conifer==0.2b0
